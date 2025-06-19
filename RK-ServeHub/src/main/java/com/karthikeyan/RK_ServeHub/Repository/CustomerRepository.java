18cbd7bf4a47b05ed683af1e5e8ea4b9c1a20381
Karthikeyan R <168166270+imrkarthikeyan@users.noreply.github.com>
2025-06-19 09:06:16 +0530

    Delete backend directory

--- RK-ServeHub/src/main/java/com/karthikeyan/RK_ServeHub/Repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.karthikeyan.RK_ServeHub.Repository;
-
-import com.karthikeyan.RK_ServeHub.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
